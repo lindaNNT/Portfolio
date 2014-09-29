dad4f30348d455d81f1b5e7f2982ad7174a53b99
Linda Nguyen Stationär <linda_NNT@hotmail.com>
2014-09-29 23:52:59 +0200

    fixat lite buggar i javaS för animeringen, ska göra klart utb. imorgon

--- LindaPortfolio/WebSite/Scripts/app/About.js
+++ LindaPortfolio/WebSite/Scripts/app/About.js
@@ -1,5 +1,7 @@
 ﻿$(function () {
     aboutSlide();
+    $('#experience-container').hide();
+    $('#education').hide();
 });
 
 function aboutSlide()
@@ -8,18 +10,26 @@ function aboutSlide()
         var rightArrow = $('#arrowL').css('display');
         var leftArrow = $('#arrowR').css('display');
 
-        if (rightArrow == 'block' && leftArrow == 'block') {
+        if (rightArrow == 'block' && leftArrow == 'block') { //egenskapsblock syns
             $('#skills-wrap').css('left', '0%');
             $('#arrowL').css('display', 'none');
             $('#arrowTextL').css('display', 'none');
             $('#arrowR').css('display', 'block');
+            $('#arrowTextR').text('Egenskaper');
+            $('#skills').hide();
+            $('#experience-container').hide();
+            $('#education').show();
         }
-        else if (rightArrow == 'block' && leftArrow == 'none') {
+        else if (rightArrow == 'block' && leftArrow == 'none') { // utbildning syns
             $('#skills-wrap').css('left', '-100%');
             $('#arrowL').css('display', 'block');
             $('#arrowTextL').css('display', 'block');
             $('#arrowR').css('display', 'block');
             $('#arrowTextR').css('display', 'block');
+            $('#arrowTextL').text('Utbildning');
+            $('#skills').show();
+            $('#experience-container').hide();
+            $('#education').hide();
         }
     });
 
@@ -27,18 +37,26 @@ function aboutSlide()
         var rightArrow = $('#arrowL').css('display');
         var leftArrow = $('#arrowR').css('display');
 
-        if (leftArrow == 'block' && rightArrow == 'block') {
+        if (leftArrow == 'block' && rightArrow == 'block') { // egenskapsblock syns
             $('#skills-wrap').css('left', '-200%');
             $('#arrowR').css('display', 'none');
             $('#arrowTextR').css('display', 'none');
             $('#arrowL').css('display', 'block');
+            $('#arrowTextL').text('Egenskaper');
+            $('#skills').hide();
+            $('#experience-container').show();
+            $('#education').hide();
         }
-        else if (rightArrow == 'none' && leftArrow == 'block') {
+        else if (rightArrow == 'none' && leftArrow == 'block') { // erfarenhet syns
             $('#skills-wrap').css('left', '-100%');
             $('#arrowR').css('display', 'block');
             $('#arrowTextR').css('display', 'block');
             $('#arrowL').css('display', 'block');
             $('#arrowTextL').css('display', 'block');
+            $('#arrowTextR').text('Erfarenheter');
+            $('#skills').show();
+            $('#experience-container').hide();
+            $('#education').hide();
         }
         
     });
